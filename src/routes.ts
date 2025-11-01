423da3189a0236350b047d2c8cd9b1dada8dd66e
Ahmad Hassan <ahmedhassan30nov@gmail.com>
2025-11-02 01:49:21 +0500

    feat: Update publicRoutes to include About and Contact pages

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 export const authRoutes = ["/auth/*"];
 
-export const publicRoutes = ["/", "/sample"];
+export const publicRoutes = ["/", "/about" , "/contact" , "/sample"];
 
 export const SIGN_IN_PAGE_PATH = "/auth/sign-in";
 export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
